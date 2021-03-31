bc0b85b24884773571ee82850a175576ad9599f6
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-31 11:33:23 +0100

    Initial commit

---
+++ MaxOccurance/CalcMax.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace MaxOccurance
+{
+    public class CalcMax
+    {
+        private readonly string _string;
+        private readonly Dictionary<char, int> LetterPairs;
+        private char HighValueKey;
+        private int HighValue;
+
+        public CalcMax(string @string) // "Computer Science"
+        {
+            _string = @string;
+            LetterPairs = new();
+            HighValue = 0;
+        }
+
+        public void PopulateDictionary()
+        {
+            for(int i = 0; i < _string.Length; i++)
+            {
+                char c = _string[i];
+                if (!LetterPairs.ContainsKey(c)) LetterPairs.Add(c, 1);
+                else if (LetterPairs.ContainsKey(c)) LetterPairs[c]++ ;
+            }
+        }
+
+        public void GetHighValueWithKey()
+        {
+            foreach(var pair in LetterPairs)
+            {
+                int x = pair.Value;
+                char c = pair.Key;
+                if (x > HighValue)
+                {
+                    HighValue = x;
+                    HighValueKey = c;
+                }
+            }
+        }
+
+        public void IsHighestValue()
+        {
+            LetterPairs.Remove(HighValueKey);
+            foreach(var pair in LetterPairs)
+            {
+
+            }
+        }
+
+        public void IsNoReoccurance()
+        {
+
+        }
+    }
+}
---
+++ MaxOccurance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MaxOccurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcMax calc = new("Computer Science");
+            calc.PopulateDictionary();
+            Console.Write(calc.CalculateMax());
+
+        }
+    }
+}
