cabb72c3c2dd583abc3f6debf8fbc8b8bfefd7c9
HoegaasianCode <80755360+HoegaasianCode@users.noreply.github.com>
2021-03-31 15:15:48 +0200

    Update Program.cs

--- MaxOccurance/Program.cs
+++ MaxOccurance/Program.cs
@@ -1,14 +1,28 @@
-﻿using System;
+using System;
 
 namespace MaxOccurance
 {
     class Program
     {
+        // https://edabit.com/challenge/vtdfueRCmpRGyLAGs
+        // UNFINISHED
+
         static void Main(string[] args)
         {
-            CalcMax calc = new("Computer Science");
+            CalcMax calc = new("Computer Science"); // "EDABIT" -> no-reoccurence-test
             calc.PopulateDictionary();
-            Console.Write(calc.CalculateMax());
+            if (!calc.IsReoccuring()) Console.Write("No Reoccurence");
+            else
+            {
+                calc.GetHighValueWithKey();
+                calc.IsHighestValue();
+                var chars = calc.GetChars();
+                foreach(var @char in chars)
+                {
+                    Console.Write(@char);
+                }
+
+            }
 
         }
     }
